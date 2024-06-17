4687cd541b25eb1b8169318b6923eb9ded5330a3
Juwon Park <devprofessionalism@gmail.com>
2024-06-17 16:44:19 +0900

    Spring Project Initialize

---
+++ src/main/java/com/example/sdui/SduiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SduiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SduiApplication.class, args);
+	}
+
+}
