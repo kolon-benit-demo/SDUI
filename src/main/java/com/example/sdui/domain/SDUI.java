903bd574547dea6ee53bb7a0f3be7283f0563a5a
Juwon Park <devprofessionalism@gmail.com>
2024-06-17 17:23:55 +0900

    feat: 엔티티 생성

---
+++ src/main/java/com/example/sdui/domain/SDUI.java
@@ -0,0 +1,28 @@
+package com.example.sdui.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "sdui")
+@NoArgsConstructor
+@Getter
+public class SDUI {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = true, columnDefinition = "LONGTEXT")
+	private String json;
+
+	public SDUI(final String json) {
+		this.json = json;
+	}
+}
